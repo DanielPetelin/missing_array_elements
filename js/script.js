4954509f996fe8b1cb047d84e71914ea14416b6a
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-30 12:26:29 +0300

    Max

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ var result = [];
 var max = mas.length;
 
 if (mas[0] < mas[1]) {
-    for (var i = mas[0]; i <= mas[mas.length - 1]; i++) {
+    for (var i = mas[0]; i <= mas[max - 1]; i++) {
         result.push(i);
     }
 } else {
-    for (var i = mas[0]; i >= mas[mas.length - 1]; i--) {
+    for (var i = mas[0]; i >= mas[max - 1]; i--) {
         result.push(i);
     }
 }
