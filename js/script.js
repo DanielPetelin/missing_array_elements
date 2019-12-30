57bc5b98f2f2ea327ca37c7b3536ce02ba94f9b1
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-30 12:13:10 +0300

    yes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
-var mas = [-4, 16];
+var mas = [40, -16];
 var result = [];
 
-for (var i = mas[0]; i <= mas[1]; i++) {
-    result.push(i);
+if (mas[0] < mas[1]) {
+    for (var i = mas[0]; i <= mas[1]; i++) {
+        result.push(i);
+    }
+} else {
+    for (var i = mas[0]; i >= mas[1]; i--) {
+        result.push(i);
+    }
 }
 
 console.log(result);
