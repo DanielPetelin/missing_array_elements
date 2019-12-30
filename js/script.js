1c83daf4241d418d5b69f05c3501d1f52f1cec47
Daniil Petelin <DaniilPetelin77@gmail.com>
2019-12-30 12:32:01 +0300

    ./

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-var mas = [-40, 23, 232];
+var mas = [40, -23, 83];
 var result = [];
 var max = mas.length;
 
-if (mas[0] < max) {
+if (mas[0] < mas[max - 1]) {
     for (var i = mas[0]; i <= mas[max - 1]; i++) {
         result.push(i);
     }
